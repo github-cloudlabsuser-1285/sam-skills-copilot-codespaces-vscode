baceff117783d9a33c25bffd42f0f609118018e4
github-cloudlabsuser-1285 <168008091+github-cloudlabsuser-1285@users.noreply.github.com>
2025-02-20 21:05:37 +0000

    files

---
+++ skills.js
@@ -0,0 +1,11 @@
+// function to convert Celsius
+// to Farenheit
+function celsiusToFarenheit(celsius) {
+    return (celsius * 9 / 5) + 32;
+}   // end of celsiusToFarenheit
+
+// Driver code
+let celsius = 37;
+console.log("Temperature in Farenheit: " + celsiusToFarenheit(celsius));
+// end of skills.js
+
